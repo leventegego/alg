b6c5934c943073eee85d3c01b0e34e48b42be957
Levente Gego <levigego02@gmail.com>
2024-06-25 14:50:07 +0200

    recursive gcd

--- src/div.cpp
+++ src/div.cpp
@@ -8,18 +8,25 @@
 
 using namespace std;
 
+// int gcd(int a, int b)
+// {
+//     tie(a, b) = minmax({ a, b });
+//     int r = a;
+//     while(b % a)
+//     {
+//         r = b % a;
+//         b = a;
+//         a = r;
+//     }
+
+//     return r;
+// }
+
 int gcd(int a, int b)
 {
-    std::tie(a, b) = std::minmax({ a, b });
-    int r = a;
-    while(b % a)
-    {
-        r = b % a;
-        b = a;
-        a = r;
-    }
-
-    return r;
+    if(a > b) swap(a, b);
+    if(a == 0) return b;
+    return gcd(a, b - a);
 }
 
 int lcm(int a, int b)
@@ -29,6 +36,14 @@ int lcm(int a, int b)
 
 int main()
 {
-
+    int n; cin >> n;
+    for(int i = 0; i < n; ++i)
+    {
+        int a, b; cin >> a >> b;
+        cout <<
+            a << " " << b << " -> " <<
+            gcd(a, b) << " " << lcm(a, b) <<
+            endl;
+    }
     return 0;
 }
